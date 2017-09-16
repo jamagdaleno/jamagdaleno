9da6742d48eed7c6ca2a02309c6a2cc2cc4ac53e
jamagdaleno <31294132+jamagdaleno@users.noreply.github.com>
2017-09-16 15:01:30 -0700

    Add files via upload

---
+++ hello-world.php
@@ -0,0 +1,12 @@
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo 'Hello world from Cloud9!';
+
+?>
+</body></html>
+
+<!--CONTACT: GOOGLE FORM is https://docs.google.com/forms/d/e/1FAIpQLSd0zNlJMz6ZvOkbxtwSnKZrLFwNytup3KV8qdsp8szUkWzyzg/viewform?usp=sf_link -->
