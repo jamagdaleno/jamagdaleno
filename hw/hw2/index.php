4eadb980331c4d608f14025b2d31a89f909950ef
jamagdaleno <31294132+jamagdaleno@users.noreply.github.com>
2017-09-19 09:11:22 -0700

    Add files via upload

---
+++ hw/hw2/index.php
@@ -0,0 +1,151 @@
+<?php
+
+echo "<h1>Homework 2</h1>";
+$checkerboard=array();
+    for($row=0;$row<10;$row++)
+    {
+    if($row%2==0)
+    {
+        for($col=0;$col<10;$col++)
+        {
+            if($col%2==0)
+            {
+                $checkerboard[$row][$col]="transparent";
+            }
+            else
+            {
+                $r = rand(0,4);
+                switch($r) {
+                    case 0:
+                        $checkerboard[$row][$col]="red";
+                        break;
+                    case 1:
+                        $checkerboard[$row][$col]="00ff18";
+                        break;
+                    case 2:
+                        $checkerboard[$row][$col]="blue";
+                        break;
+                    case 3:
+                        $checkerboard[$row][$col]="yellow";
+                        break;
+                    case 4:
+                        $checkerboard[$row][$col]="fc00ff";
+                        break;
+                }
+            }
+        }
+    }
+    else
+    {
+        for($col=0;$col<10;$col++)
+        {
+            if($col%2==0)
+            {
+                $r = rand(0,2);
+                switch($r) {
+                    case 0:
+                        $checkerboard[$row][$col]="red";
+                        break;
+                    case 1:
+                        $checkerboard[$row][$col]="00ff18";
+                        break;
+                    case 2:
+                        $checkerboard[$row][$col]="blue";
+                        break;
+                }
+            }
+            else
+            {
+                $checkerboard[$row][$col]="transparent";
+            }
+        }
+    }
+}
+$diagonal=array();
+    for($row=0;$row<10;$row++)
+    {
+        for($col=0;$col<10;$col++)
+        {
+            if($row==$col)
+            {
+                $diagonal[$row][$col]='transparent';
+            }
+            else
+            {
+                $diagonal[$row][$col]='transparent';
+            }
+            if($row+$col==9)
+            {
+                $diagonal[$row][$col]='transparent';
+            }
+        }
+    }
+echo "<table>";
+for($row=0;$row<count($checkerboard);$row++)
+{
+    echo "<tr>";
+        for($col=0;$col<count($checkerboard);$col++)
+        {
+            echo "<td width='50px' height='50px' bgcolor='".$checkerboard[$row][$col]."'></td>";
+        }
+    echo "</tr>";
+}
+echo "</table>";
+echo "<br>";
+
+echo "<h2>SORT:</h2>";
+echo "<br>";
+
+sort($checkerboard);
+
+echo "<br />";
+
+echo "<table>";
+for($row=0;$row<count($checkerboard);$row++)
+{
+    echo "<tr>";
+        for($col=0;$col<count($checkerboard);$col++)
+        {
+            echo "<td width='50px' height='50px' bgcolor='".$checkerboard[$row][$col]."'></td>";
+        }
+    echo "</tr>";
+}
+echo "</table>";
+echo "<br>";
+
+echo "<h2>SHUFFLE:</h2>";
+echo "<br>";
+
+shuffle($checkerboard);
+echo "<br />";
+
+echo "<table>";
+for($row=0;$row<count($checkerboard);$row++)
+{
+    echo "<tr>";
+        for($col=0;$col<count($checkerboard);$col++)
+        {
+            echo "<td width='50px' height='50px' bgcolor='".$checkerboard[$row][$col]."'></td>";
+        }
+    echo "</tr>";
+}
+echo "</table>";
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Assignment 2</title>
+
+        <link rel="stylesheet" href="styles.css">
+        
+    </head>
+    <body>
+    </body>
+</html>
