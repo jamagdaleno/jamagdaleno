32b0f0c850eadd4586091a0a54c6573cedb6acb5
jamagdaleno <31294132+jamagdaleno@users.noreply.github.com>
2017-10-15 23:03:48 -0700

    Add files via upload

--- labs/lab5/getAuthorInfo.php
+++ labs/lab5/getAuthorInfo.php
@@ -1,28 +1,33 @@
-<?php
-
-include '../../../dbConnection.php';
-
-$conn = getDatabaseConnection();
-
-$sql = "SELECT * FROM `q_author` WHERE authorId=" . $_GET['authorId'];
-$stmt = $conn -> prepare ($sql);
-$stmt -> execute();
-$record = $stmt -> fetch();
-
-echo $record['firstName'] . "  " . $record['biography'];
-
-?>
-
 <!DOCTYPE html>
 <html>
-    <head>
-        <title> Author Info </title>
-    </head>
-    <body>
+<head>
+<title> Author Info </title>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
 
-<h1> Author Info </h1>
+<body>
+<?php
 
+    include 'dbConnection.php';
 
+    $conn = getDatabaseConnection();
 
-    </body>
+    $sql = "SELECT * FROM author WHERE authorid=" . $_GET['authorid'];
+    $stmt = $conn -> prepare ($sql);
+    $stmt -> execute();
+    $record = $stmt -> fetch();
+
+    echo "img src='" . $record['picture'] . "' width='200px'>";
+    
+    echo "<div><span class='bolder'>Name: </span>" . $record['firstName'] . " " . $record['lastName'] . "</div>";
+    echo "<div><span class='bolder'>Gender: </span>" . $record['gender'] . "</div>";
+    echo "<div><span class='bolder'>Date of Birth: </span>" . $record['dob'] . "</div>";
+    echo "<div><span class='bolder'>Date of Death: </span>" . $record['dod'] . "</div>";
+    echo "<div><span class='bolder'>Profession: </span>" . $record['profession'] . "</div>";
+    echo "<div><span class='bolder'>Country: </span>" . $record['country'] . "</div>";
+    echo "<div><span class='bolder'>Biography: </span>" . $record['biography'] . "</div>";
+    echo "<br>";
+    
+?>
+</body>
 </html>
