54eaa058703305c1ee35b43a106be1bdfdf09c2f
jamagdaleno <31294132+jamagdaleno@users.noreply.github.com>
2017-10-31 12:55:42 -0700

    Add files via upload

---
+++ labs/lab7/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Lab 7: Admin Login</title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <body>
+
+        <br><br><br><br><br><br>
+        <h1> Admin Login</h1>
+        
+        <form method="POST" action="loginProcess.php">
+    
+        Username: <input type="text" name="username"/> <br />
+        &nbsp;Password: <input type="password" name="password"/> <br />
+        <input type="submit" value="Login!" name="loginForm" />
+            
+        </form>
+
+
+    </body>
+</html>
