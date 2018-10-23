93f879bca8003a233a73255ff9bf84237f68c981
jamagdaleno <31294132+jamagdaleno@users.noreply.github.com>
2018-10-22 18:21:52 -0700

    Add files via upload

---
+++ sqlConnection.php
@@ -0,0 +1,23 @@
+<?php
+function getConnection($dbname) {
+    
+        $host = "localhost";  //c9
+        //$dbname = "quotes";
+        $username = "root";
+        $password = "";
+        
+        //when connecting from Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    }     
+        
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+        return $dbConn;
+}
+?>
